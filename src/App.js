0214e01da8a479efec4bf99e95b006ef48dbb96d
Alex Dunae <alex@dunae.ca>
2020-12-29 11:19:31 -0800

    Better heartrate display

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import HeartRate from './HeartRate'
 
 // Note: passing in integers, not dates, to simplify comparison
 const HEART_RATE_MEASUREMENTS = [
-  {timestamp: Date.parse('2020-01-01'), bpm: 60},
-  {timestamp: Date.parse('2020-02-01'), bpm: 55},
-  {timestamp: Date.parse('2020-03-01'), bpm: 50},
-  {timestamp: Date.parse('2020-04-01'), bpm: 70}
+  {timestamp: Date.parse('2020-01-01'), value: 60},
+  {timestamp: Date.parse('2020-02-01'), value: 55},
+  {timestamp: Date.parse('2020-03-01'), value: 50},
+  {timestamp: Date.parse('2020-04-01'), value: 70}
 ]
 
 function App() {
--- src/HeartRate.js
+++ src/HeartRate.js
@@ -1,6 +1,19 @@
 const UNITS = 'bpm'
 const DISPLAY_COUNT = 10;
 
+
+// this would likely be exported from a shared file
+function formatTimestamp(timestamp) {
+    // some error checking, or Typescript
+    const date = new Date(timestamp)
+    const display = date.toLocaleDateString()
+    const attr = date.toISOString()
+
+    // NB React attribute case
+    return <time dateTime={attr}>{display}</time>
+
+}
+
 function HeartRate(props) {
     const measurements = props.measurements || [];
 
@@ -12,18 +25,22 @@ function HeartRate(props) {
         )
     }
 
-    const dataPoints = measurements.sort((a, b) => a.timestamp - b.timestamp).slice(0, DISPLAY_COUNT).map(measurement => {
+    // sort oldest to newest
+    const dataPoints = measurements.sort((a, b) => b.timestamp - a.timestamp).slice(0, DISPLAY_COUNT).map(measurement => {
         return (
         <li className="stats-measurement" key={measurement.timestamp}>
-            <span className="stats-timestamp">{measurement.timestamp}</span>
-            {measurement.amount} {UNITS}
+            <span className="stats-timestamp">{formatTimestamp(measurement.timestamp)}</span>
+            {' '}
+            <span className="stats-value">{measurement.value} {UNITS}</span>
         </li>)
     })
 
     return (
         <div className="stats stats-heartrate">
             <h3>Heart Rate</h3>
+            <ul className='stats-measurements'>
         {dataPoints}
+        </ul>
         </div>
     )
 }
